5051c058b02f8b84c7ce6d04515e8a0e5572f335
Denis Stoyanouski <vandenst@tut.by>
2022-10-18 21:46:48 +0300

    add class Command

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,7 +1,10 @@
 package server;
 
-public class Main {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Main {
+    Set<String> server = new HashSet<>();
     public static void main(String[] args) {
         run();
     }
@@ -9,4 +12,22 @@ public class Main {
     private static void run() {
 
     }
+}
+
+enum Command {
+    ADD("add"),
+    GET("get"),
+    DELETE("delete"),
+    EXIT("exit");
+
+    String name;
+
+    Command (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
