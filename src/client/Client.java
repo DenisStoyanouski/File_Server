d0f16bf99989cca500d9a972fc4a7cc18b22c658
Denis Stoyanouski <vandenst@tut.by>
2022-10-21 19:17:17 +0300

    add methods getInput and serve to class Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,28 +5,66 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.sql.SQLOutput;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Client {
     private static final String SERVER_ADDRESS = "127.0.0.1";
     private static final int SERVER_PORT = 23456;
 
+    private static final StringBuilder request = new StringBuilder();
+
     public static void main(String[] args) {
-        System.out.println("Client started!");
+
+        System.out.print("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): ");
+        String action = getInput();
+        switch (action) {
+            case "1" : request.append("GET ");
+            break;
+            case "2" : request.append("CREATE ");
+            break;
+            case "3" : request.append("DELETE ");
+            break;
+            case "exit" : request.append("EXIT");
+            break;
+            default :
+                System.out.println("Unknown action");
+                break;
+        }
+        if (!request.toString().contains("EXIT")) {
+            System.out.print("Enter filename: ");
+            request.append(getInput());
+            request.append(" ");
+            if (request.toString().contains("GET")) {
+                System.out.print("Enter file content: ");
+                request.append(getInput());
+            }
+        }
+
+
+    }
+
+    private static String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private static String serve(String request) {
+        String response = null;
         try (
                 Socket socket = new Socket(InetAddress.getByName(SERVER_ADDRESS), SERVER_PORT);
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
         ) {
-            //for (int i = 0; i < 5; i++) {
-            //Scanner scanner = new Scanner(System.in);
-            String msg = "Give me everything you have!";// scanner.nextLine();
-            System.out.printf("Sent: %s%n", msg);
-            output.writeUTF(msg);
-            String receivedMsg = input.readUTF();
-            System.out.printf("Received: %s%n",receivedMsg);
+            output.writeUTF(request);
+            System.out.println("The request was sent.");
+            response = input.readUTF();
+
             //}
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return response;
     }
 }
